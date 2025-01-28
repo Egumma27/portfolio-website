18b5d33388ffd992661df529ceaedb0aeda58f26
EgummaWayne <gummae@icloud.com>
2025-01-28 00:56:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Welcome to My Resume Website bleh</h1>
+      <h1>Welcome to My Resume Website bleh idk what to add</h1>
       <p>This is a work in progress!</p>
     </div>
   );
